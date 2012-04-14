b1006d70167abc9d21a722137477b5cf995c7ca6
Matthias Fuchs <matz@linux-esio.site>
2012-04-14 13:12:00 +0200

    Added SetParameterCmd

--- src/RemoveOperatorCmd.cpp
+++ src/RemoveOperatorCmd.cpp
@@ -3,7 +3,7 @@
 #include "StreamModel.h"
 
 RemoveOperatorCmd::RemoveOperatorCmd(StreamModel* stream, OperatorModel* model, QUndoCommand* parent)
-  : QUndoCommand(parent),
+  : QUndoCommand(QObject::tr("remove operator"), parent),
     m_stream(stream),
     m_model(model)
 {
