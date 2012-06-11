4994702c647fd8048e3d38d61f0694b637676fe3
Matthias Fuchs <matz.fuchs@gmx.at>
2012-06-11 20:28:57 +0430

    Return values to fix Windows build

--- src/ErrorListModel.cpp
+++ src/ErrorListModel.cpp
@@ -7,21 +7,21 @@ ErrorListModel::ErrorListModel(QObject* parent)
 
 int ErrorListModel::columnCount(const QModelIndex& parent) const
 {
+    return 0;
 }
 
 int ErrorListModel::rowCount(const QModelIndex& parent) const
 {
-}
-
-Qt::ItemFlags ErrorListModel::flags(const QModelIndex& index) const
-{
+    return 0;
 }
 
 QVariant ErrorListModel::headerData(int section, Qt::Orientation orientation, int role) const
 {
+    return QVariant();
 }
 
 QVariant ErrorListModel::data(const QModelIndex& index, int role) const
 {
+    return QVariant();
 }
 
--- src/ErrorListModel.h
+++ src/ErrorListModel.h
@@ -42,7 +42,6 @@ public:
     virtual int rowCount(const QModelIndex & parent) const;
     virtual int columnCount(const QModelIndex & parent) const;
     virtual QVariant data(const QModelIndex & index, int role = Qt::DisplayRole) const;
-    virtual Qt::ItemFlags flags(const QModelIndex & index) const;
     virtual QVariant headerData(int section, Qt::Orientation orientation, int role) const;
     
 private:
